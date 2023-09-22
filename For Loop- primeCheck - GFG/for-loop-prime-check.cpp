a1333c188a0c43a8b0c645d84f1565ec7ed1f532
namratakaushik016 <118747968+namratakaushik016@users.noreply.github.com>
2023-09-22 10:53:38 +0530

    Added solution - LeetHub

---
+++ For Loop- primeCheck - GFG/for-loop-prime-check.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Complete this function
+string isPrime(int n) {
+    if(n==1 || n==0){
+        return "No";
+    }
+    if(n==2){
+        return "Yes";
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+
+    
+    if(n%i==0){
+        return "No";
+    }
+    else{
+        continue;
+    }
+    
+    
+    }
+    return "Yes";
+}
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        cout << isPrime(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
